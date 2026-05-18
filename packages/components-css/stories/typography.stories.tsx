3b383c3dacfce1925d9da88f774537e962af2cc9
Richard Davies <richard.davies@portlandoregon.gov>
2026-05-18 21:11:50 +0000

    PGOV-2045 Add space lipsum paragraph text under each heading

--- packages/components-css/stories/typography.stories.tsx
+++ packages/components-css/stories/typography.stories.tsx
@@ -22,9 +22,20 @@ export const Utility_Classes: Story = {
         <h2 className="heading-lg">Utility Heading Classes</h2>
         <div className="flex flex-col gap-sm">
           {Object.entries(base["font-size"].heading).map(([key]) => (
-            <h2 key={key} className={`heading-${key}`}>
-              City of Portland | <code>heading-{key}</code>
-            </h2>
+            <>
+              <h2 key={key} className={`heading-${key}`}>
+                City of Portland | <code>heading-{key}</code>
+              </h2>
+              <p>
+                We choose to go to the moon. We choose to go to the moon in this
+                decade and do the other things, not because they are easy, but
+                because they are hard, because that goal will serve to organize
+                and measure the best of our energies and skills, because that
+                challenge is one that we are willing to accept, one we are
+                unwilling to postpone, and one which we intend to win, and the
+                others, too.
+              </p>
+            </>
           ))}
         </div>
       </div>
