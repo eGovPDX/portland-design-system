1baae7558fedc18a4b5256a4b5b225636c90a01e
radmosley <ronnie.j.mosley@gmail.com>
2026-07-09 23:12:07 +0000

    pgov-1492 cleaning branch

--- packages/components-react/src/header/header.tsx
+++ packages/components-react/src/header/header.tsx
@@ -62,7 +62,7 @@ export const HeaderNavLink: React.FC<ReactHeaderNavLinkProps> = ({
   className = "",
   ...rest
 }) => {
-  const classes = ["header__nav-link", className].filter(Boolean).join(" ");
+  const classes = mergeClasses(["link", className]);
 
   return (
     <a href={href} className={classes} {...rest}>
