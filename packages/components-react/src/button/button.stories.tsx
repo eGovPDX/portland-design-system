095d4dc8422d4b371383a99065e54cbb0e76435d
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2026-07-18 08:11:43 +0000

    refactor(react): Remove contrast check from button stories
    ref: PGOV-1534

--- packages/components-react/src/button/button.stories.tsx
+++ packages/components-react/src/button/button.stories.tsx
@@ -2,18 +2,14 @@ import { BUTTON_SIZES, BUTTON_TYPES } from "@cityofportland/types/button";
 import {
   BOX_COLORS,
   BOX_VARIANTS,
+  validateBoxConfiguration,
   type BoxColorScheme,
   type BoxColorVariation,
 } from "@cityofportland/types/box";
-import {
-  faArrowLeft,
-  faArrowRight,
-  faEllipsis,
-} from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import Color from "colorjs.io";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { fn } from "storybook/test";
 
 import { Box } from "../box";
@@ -207,10 +203,10 @@ export const Variants: StoryObj<
     },
   },
   args: {
-    backgroundColor: "default",
-    backgroundVariant: "subtle",
     color: "default",
     size: "md",
+    backgroundColor: "default",
+    backgroundVariant: "subtle",
   },
   parameters: {
     layout: "fullscreen",
@@ -228,112 +224,48 @@ export const Variants: StoryObj<
     },
   },
   render({ backgroundColor, backgroundVariant, color, outline, ...props }) {
-    const [backgroundElement, setBackgroundElement] =
-      useState<HTMLElement | null>(null);
-    const [bgColor, setBgColor] = useState<Color>();
-
-    const getElement = (id: string) => document.querySelector(`#${id}`);
-
-    const getProperty = (id: string, name: string) => {
-      const element = getElement(id);
-
-      if (!element) {
-        return null;
-      }
-
-      return getComputedStyle(element).getPropertyValue(name).trim();
-    };
-
-    useEffect(() => {
-      if (!backgroundElement) return;
-      const style = getComputedStyle(backgroundElement);
-      setBgColor(new Color(style.backgroundColor));
-    }, [backgroundElement, backgroundColor, backgroundVariant, color]);
-
     const ButtonTester = ({ color, variant }) => {
       const id = `${color}-${variant}`;
-      const el = getElement(id);
-
-      const b = getProperty(id, "--box-background-color");
-      const bc =
-        bgColor && b && Color.contrastWCAG21(bgColor, new Color(b)).toFixed(2);
-      const r =
-        getProperty(id, "--box-ring-color") ||
-        (el && getComputedStyle(el).color);
-      const rc =
-        bgColor && r && Color.contrastWCAG21(bgColor, new Color(r)).toFixed(2);
-
-      const o = getProperty(id, "--box-outline-color");
-      const oc =
-        bgColor && o && Color.contrastWCAG21(bgColor, new Color(o)).toFixed(2);
 
       return (
         <div className="grid items-start gap-md">
           <span key={`${variant}-label`} className="capitalize font-bold">
             {variant}
           </span>
-          <div className="grid md:grid-cols-2 gap-md">
-            <DemoButton
-              key={`${variant}`}
-              id={id}
-              color={color}
-              variant={variant}
-              outline={outline}
-              className="capitalize "
-              {...props}
-            >
-              {variant}
-            </DemoButton>
-            <dl className="grid sm:grid-cols-3 gap-xs">
-              {outline && r && (
-                <div className="flex-1 grid gap-xs">
-                  <dt className="font-bold">outline</dt>
-                  <dd>
-                    {new Color(r).toString({ format: "hex" })} ({rc}){" "}
-                    {Number(rc) >= 3 ? "✅" : "❌"}
-                  </dd>
-                </div>
-              )}
-              {!outline && (
-                <div className="flex-1 grid gap-xs">
-                  <dt className="font-bold">background</dt>
-                  <dd>
-                    {b} ({bgColor && bc}){" "}
-                    {bgColor && Number(bc) >= 3 ? "✅" : "❌"}
-                  </dd>
-                </div>
-              )}
-              <div className="flex-1 grid gap-xs">
-                <dt className="font-bold">focus</dt>
-                <dd>
-                  {o} ({oc}) {Number(oc) >= 3 ? "✅" : "❌"}
-                </dd>
-              </div>
-            </dl>
-          </div>
+
+          <DemoButton
+            key={`${variant}`}
+            id={id}
+            color={color}
+            variant={variant}
+            outline={outline}
+            className="capitalize "
+            {...props}
+          >
+            {variant}
+          </DemoButton>
         </div>
       );
     };
 
     return (
       <Box
-        ref={setBackgroundElement}
         as="section"
         color={backgroundColor}
         variant={backgroundVariant}
         className="min-h-screen p-xl"
       >
-        <span>current background: {bgColor?.toString({ format: "hex" })}</span>
-        <h2 className="heading-lg capitalize">Unset</h2>
-        <ButtonTester color="unset" variant="unset" />
-
         <div className="grid grid-cols-1 gap-lg items-center">
           <h2 key={color} className="heading-lg capitalize">
             {color}
           </h2>
-          {BOX_VARIANTS.map((variant) => (
-            <ButtonTester color={color} variant={variant} />
-          ))}
+          {BOX_VARIANTS.map((variant) =>
+            validateBoxConfiguration(color, variant)
+          )
+            .filter(([color, variant]) => color && variant)
+            .map(([color, variant]) => (
+              <ButtonTester color={color} variant={variant} />
+            ))}
         </div>
       </Box>
     );
@@ -413,34 +345,6 @@ export const Incognito: StoryObj<StoryProps & { underline: boolean }> = {
         </DemoButton>
         .
       </p>
-    </div>
-  ),
-};
-
-export const Stacked: Story = {
-  parameters: {
-    layout: "fullscreen",
-    controls: {
-      exclude: ["children", "className", "onClick"],
-    },
-  },
-  render: ({ color, variant, size, ...props }) => (
-    <div className="flex flex-col items-center justify-center min-h-screen max-w-screen-sm mx-auto gap-md">
-      <Box
-        as="div"
-        color={color}
-        variant={variant}
-        className={[
-          "pl-xs flex items-center justify-between gap-md hover:bg-(--box-background-color)",
-        ]
-          .filter(Boolean)
-          .join(" ")}
-      >
-        <span className="underline">Some text leading in</span>
-        <DemoButton size={size} className="h-full" {...props}>
-          <FontAwesomeIcon icon={faEllipsis} />
-        </DemoButton>
-      </Box>
     </div>
   ),
 };
